331632ab772e779c7186659b4c00c2b1c9ec908f
ThorZol <58010547+sergunchik218@users.noreply.github.com>
2023-08-16 10:19:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace XDoc_text1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lable1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lable2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(41, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Отчет с ошибками";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lable1
+            // 
+            this.lable1.AutoSize = true;
+            this.lable1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lable1.Location = new System.Drawing.Point(258, 55);
+            this.lable1.Name = "lable1";
+            this.lable1.Size = new System.Drawing.Size(38, 15);
+            this.lable1.TabIndex = 1;
+            this.lable1.Tag = "lable1";
+            this.lable1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Исходный отчет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lable2
+            // 
+            this.lable2.AutoSize = true;
+            this.lable2.Location = new System.Drawing.Point(258, 130);
+            this.lable2.Name = "lable2";
+            this.lable2.Size = new System.Drawing.Size(38, 15);
+            this.lable2.TabIndex = 3;
+            this.lable2.Tag = "lable2";
+            this.lable2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(41, 215);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Go and Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-36, -8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(876, 525);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 516);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lable2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lable1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Tag = "lable1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lable1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lable2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+
+namespace XDoc_text1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        XDocument text1;
+        XDocument text2;
+        XDocument text3;
+        //XDocument text3 = new XDocument(new XDeclaration("1.0", "utf-8", "yes"));
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "XML-файл |*.xml|All files|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lable1.Text = openFileDialog1.FileName;
+                text1 = XDocument.Load(lable1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "XML-файл |*.xml|All files|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lable2.Text = openFileDialog1.FileName;
+                text2 = XDocument.Load(lable2.Text);
+                text3 = text2;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<string> cod = new List<string>();
+            foreach (var o2 in text2.Descendants()?.Elements("sscc")) cod.Add(o2.Value);
+            foreach (var o1 in text1.Descendants()?.Elements("object_id")) cod.Remove(o1.Value);
+            var union3 = text3.Descendants()?.Elements("union");
+            
+            //MessageBox.Show(union3.ToString());
+            //if (union3.ToString() !="")        //Условие на удаление Юниона
+            
+            foreach (var cod1 in cod)
+            {
+                foreach (var un in union3)
+                {
+                    if (un.Value.Contains(cod1))
+                    {
+                        un.Remove();
+                        break;
+                    }
+                }
+            } 
+            //условие на удаление sscc
+            //MessageBox.Show("Putin");
+            var sscc3 = text3.Descendants()?.Elements("sscc");
+            //foreach (var un1 in sscc3)
+            //MessageBox.Show(un1.Value);
+            foreach (var cod1 in cod)
+            {
+                foreach (var un in sscc3)
+                {
+                    if (un.Value.Contains(cod1))
+                    {
+                        un.Remove();
+                        break;
+                    }
+                }
+            }
+            saveFileDialog1.Filter = "XML-файл |*.xml|All files|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                text3.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
